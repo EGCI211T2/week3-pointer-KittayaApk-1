923dfa021fd348cf8e5917bb6bf55a2e36c7c031
KittayaApk <kittaya.apk@student.mahidol.edu>
2026-01-28 04:31:22 +0000

    finish

--- main.cpp
+++ main.cpp
@@ -4,19 +4,12 @@
 using namespace std;
 
 int main(int argc, char *argv[])
-{
-    int *pa, *pb, temp, i;
-
-    cout << "argc is " << argc << endl;
-
+{ 
     int n = argc - 1;
-    if (n <= 0) return 0;
-
-    pa = new int[n];
+    int *pa = new int[n];
     int *start = pa;
 
-    // convert argv to int (manual, no atoi)
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         int value = 0;
         int j = 0;
         while (argv[i + 1][j] != '\0') {
@@ -27,32 +20,23 @@ int main(int argc, char *argv[])
         pa++;
     }
 
+    // reverse
     pa = start;
-    cout << "Original: ";
-    for (i = 0; i < n; i++) {
-        cout << setw(3) << *pa;
-        pa++;
-    }
-    cout << endl;
-
-    pa = start;
-    pb = start + n - 1;
-
-    for (i = 0; i < n / 2; i++) {
-        temp = *pa;
+    int *pb = start + n - 1;
+    for (int i = 0; i < n / 2; i++) {
+        int temp = *pa;
         *pa = *pb;
         *pb = temp;
         pa++;
         pb--;
     }
 
+  
     pa = start;
-    cout << "Reversed: ";
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         cout << setw(3) << *pa;
         pa++;
     }
-    cout << endl;
 
     delete[] start;
     return 0;
